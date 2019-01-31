53d59d9b59ae3f5e3be3e48609ad9d778fd8d47f
jengelstengel <twan.mulder@gmail.com>
2019-01-31 13:14:41 +0100

    Updated index.js
    Added connection confirmation and simple command that can be used in chat to reply to a user.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const tmi - require('tmi.js');
+const tmi = require('tmi.js');
 
-const options - {
+const options = {
   options: {
     debug: true,
   },
@@ -11,5 +11,20 @@ const options - {
   identity: {
     username: 'TestBot1',
     password: 'oauth:3hli0fd76gw628wgr1p2ruc0janvos',
-  }
+  },
+  channels: ['jengelstengel'],
 };
+
+const client = new tmi.client(options);
+
+client.connect();
+
+client.on('connected', (address, port) => {
+  client.action('jengelstengel', 'Hello, TestBot1 is now connected');
+});
+
+client.on('chat', (channel, user, message, self) => {
+  if (message === '!test') {
+    client.action('jengelstengel', 'This is a test message!');
+  }
+});
